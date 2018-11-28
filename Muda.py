70a79e575e3c24434ab1fc707b1582e7454bfae9
Fernando J. Alarcon <jesusalarcon10@gmail.com>
2018-11-28 14:12:45 -0700

    vesion 0.0.1

---
+++ Muda.py
@@ -0,0 +1,7 @@
+'''The soul purpose of this file was to push it to github.
+   To remind me how to use Git hub again.
+'''
+
+print("~~~~~Usless python file but helps me git familiar with github~~~~~")
+
+input("Press Enter to close")
